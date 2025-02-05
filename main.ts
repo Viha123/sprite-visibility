5a55420c4c341397d5345585d27e65bc0febe4c4
Viha Shah <47764272+Viha123@users.noreply.github.com>
2025-02-04 21:37:13 -0800

    :sparkles: working code for both tilemap and background iamges!

--- main.ts
+++ main.ts
@@ -1,20 +1,15 @@
-/**
- * Functions are mapped to blocks using various macros
- * in comments starting with %. The most important macro
- * is "block", and it specifies that a block should be
- * generated for an **exported** function.
- */
-
-//% color="#AA278D" weight=100
 namespace sprite_visibility {
 
     let bImage = scene.backgroundImage();
 
     let tmap = game.currentScene().tileMap;
+    let tmapd: tiles.TileMapData;
+    let currentTileSet: Image[];
     //% block
     export function setBackgroundImage(bg: any) {
         bImage = bg;
     }
+
     //% block
     export function coverAroundSpriteBackgroundImage(sprite: Sprite, radius: number) {
         let spriteX = sprite.x;
@@ -42,35 +37,19 @@ namespace sprite_visibility {
     // we know in 
     //% block
     export function coverAroundSpriteTileMap(sprite: Sprite, visibileTile: number) {
-        let spriteX = sprite.x;
-        let spriteY = sprite.y;
-        let tmap = game.currentScene().tileMap;
-        let pixelXSize = tmap.areaWidth() / tmap.data.width;
-        let pixelYSize = tmap.areaHeight() / tmap.data.height;
-        let spriteRow = Math.floor(spriteY / pixelYSize);
-        let spriteCol = Math.floor(spriteX / pixelXSize);
-        // let bg = scene.backgroundImage();
-        // console.log(bg.getPixel(spriteX, spriteY));
-        // // console.log(spriteX);
-        // // console.log(bg.getPixel(10,10));
-        let w = screen.width;
-        let h = screen.height;
-
-        console.log(game.currentScene().tileMap.data.width);
-        console.log(game.currentScene().tileMap.data.height);
-        // for(let r = 0; r < h; r ++) {
-        //     for(let c = 0; c < w; c++) {
-        //         if(Math.sqrt(Math.pow(spriteX - c,2) + Math.pow(spriteY - r, 2)) > radius) {
-        //             bg.setPixel(c, r, 0); //set it to black 
-        //         } else {
-        //             bg.setPixel(c, r, tmap.getPixel(c,r));
-        //         }
-        //     }
-        // }
-        for (let r = spriteRow - 4; r < spriteRow + 4; r++) {
-            for (let c = spriteCol - 5; c < spriteCol + 5; c++) {
-                if (Math.abs(r - spriteRow) > visibileTile || Math.abs(c - spriteCol) > visibileTile) {
-                    tiles.setTileAt(tiles.getTileLocation(c, r), img`
+        let spriteX2 = sprite.x;
+        let spriteY2 = sprite.y;
+        let tmap2 = game.currentScene().tileMap;
+        let pixelXSize = tmap2.areaWidth() / tmap2.data.width;
+        let pixelYSize = tmap2.areaHeight() / tmap2.data.height;
+        let spriteRow = Math.floor(spriteY2 / pixelYSize);
+        let spriteCol = Math.floor(spriteX2 / pixelXSize);
+        let a = screen.width;
+        let i = screen.height;
+        for (let s = spriteRow - 4; s < spriteRow + 5; s++) {
+            for (let d = spriteCol - 5; d < spriteCol + 6; d++) {
+                if (Math.abs(s - spriteRow) > visibileTile || Math.abs(d - spriteCol) > visibileTile) {
+                    tiles.setTileAt(tiles.getTileLocation(d, s), img`
 f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f 
@@ -88,13 +67,20 @@ f f f f f f f f f f f f f f f f
 f f f f f f f f f f f f f f f f 
 f f f f f f f f f f f f f f f f 
 `);
-
+                } else {
+                    tiles.setTileAt(tiles.getTileLocation(d, s), currentTileSet[tmapd.getTile(d, s)]);
                 }
             }
         }
     }
 
-
+    //% block
+    export function setTileMap(tm: tiles.TileMapData) {
+        // tmap = tm;
+        // tmap = tm;
+        tmapd = tm;
+        currentTileSet = tm.getTileset();
+    }
 
     // note that Caml casing yields lower case
     // block text with spaces
